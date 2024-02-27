21e8f91546f02944236d80069ed7189a27ea898f
Filip Sakala <filip.sakala@vacuumlabs.com>
2024-02-27 16:42:39 +0100

    feat: init material ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import studyBuddy from "./assets/study_buddy.png";
+import studyBuddy2 from "./assets/study_buddy2.png";
+import "./App.css";
+import { TrendingUp } from "@mui/icons-material";
+import { styled } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+const StatWrapper = styled("div")(
+  ({ theme }) => `
+  background-color: ${theme.palette.background.paper};
+  box-shadow: ${theme.shadows[1]};
+  border-radius: ${theme.shape.borderRadius}px;
+  padding: ${theme.spacing(2)};
+  min-width: 300px;
+`
+);
+
+const StatHeader = styled("div")(
+  ({ theme }) => `
+  color: ${theme.palette.text.secondary};
+`
+);
+
+const StyledTrend = styled(TrendingUp)(
+  ({ theme }) => `
+  color: ${theme.palette.success.dark};
+  font-size: 16px;
+  vertical-alignment: sub;
+`
+);
+
+const StatValue = styled("div")(
+  ({ theme }) => `
+  color: ${theme.palette.text.primary};
+  font-size: 34px;
+  font-weight: ${theme.typography.fontWeightMedium};
+`
+);
+
+const StatDiff = styled("div")(
+  ({ theme }) => `
+  color: ${theme.palette.success.dark};
+  display: inline;
+  font-weight: ${theme.typography.fontWeightMedium};
+  margin-left: ${theme.spacing(0.5)};
+  margin-right: ${theme.spacing(0.5)};
+`
+);
+
+const StatPrevious = styled("div")(
+  ({ theme }) => `
+  color: ${theme.palette.text.secondary};
+  display: inline;
+  font-size: 12px;
+`
+);
+
+const App = () => {
+  const [count, setCount] = useState<number>(100);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={studyBuddy} className="logo" alt="Study buddy" />
+        <img src={studyBuddy2} className="logo" alt="Study sources" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Study Buddy Español</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount(() => Math.floor(Math.random() * 100))}>
+          count is not {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <StatWrapper>
+        <StatHeader>Sessions</StatHeader>
+        <StatValue>98.3 K</StatValue>
+        <StyledTrend />
+        <StatDiff>18.77%</StatDiff>
+        <StatPrevious>vs last week</StatPrevious>
+      </StatWrapper>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
