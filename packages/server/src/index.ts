46b00fde4fae6bd964acf571220557fc315e7f09
Filip Sakala <filip.sakala@vacuumlabs.com>
2024-03-29 09:03:50 +0100

    feat: apply cors origin policy

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -6,7 +6,12 @@ import answersApi from "./answersApi";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://espanol.sakala.me", "http://localhost:5173"],
+    methods: "GET,HEAD,POST",
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/question", questionsApi);
