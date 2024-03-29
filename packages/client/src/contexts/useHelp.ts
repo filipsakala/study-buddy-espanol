51b13f14162eb67ceace6321b272103e5f8c6e11
Filip Sakala <filip.sakala@vacuumlabs.com>
2024-03-29 16:13:24 +0100

    refactor: help actions

---
+++ packages/client/src/contexts/useHelp.ts
@@ -0,0 +1,71 @@
+import { useCallback, useMemo, useState } from "react";
+import { Question } from "../types/Question";
+
+const produceSpacedArray = (word: string): string[] => {
+  return [...Array(word.length)].map((_, i) => {
+    // duplicate spaces between words
+    if (word[i] === " ") {
+      return " ";
+    }
+    return "";
+  });
+};
+
+// Help for a word is an array of letters for the correct answer
+// User can repeatedly ask for another random letter
+const useHelp = (
+  currentQuestion: Question,
+  setCurrentQuestionScore: (
+    isCorrectAnswer: boolean,
+    withHelp: boolean
+  ) => void,
+  goToNextQuestion: () => void
+) => {
+  const [help, setHelp] = useState<string[]>([]);
+
+  const resetHelp = useCallback(() => setHelp([]), []);
+
+  const emptyIndexes = useMemo(() => {
+    return help.reduce((prevIndexes: number[], currentHelp, currentIndex) => {
+      const nextIndexes = [...prevIndexes];
+
+      if (!currentHelp) {
+        nextIndexes.push(currentIndex);
+      }
+
+      return nextIndexes;
+    }, []);
+  }, [help]);
+
+  const getQuestionHelp = useCallback(() => {
+    const correctAnswer = currentQuestion.correctAnswer;
+    // just show number of letters for given answer
+    if (!help.length) {
+      setHelp(produceSpacedArray(correctAnswer));
+      return;
+    }
+    // all indexes already filled, nothing to do
+    if (emptyIndexes.length <= 1) {
+      setCurrentQuestionScore(false, true);
+      goToNextQuestion();
+      resetHelp();
+      return;
+    }
+    // get random index from the answer and set it as non-empty
+    const randomIndexInRange = Math.floor(Math.random() * emptyIndexes.length);
+    const randomEmptyIndex = emptyIndexes[randomIndexInRange];
+
+    setHelp((prev) => {
+      const next = [...prev];
+      next[randomEmptyIndex] = correctAnswer[randomEmptyIndex];
+      return next;
+    });
+  }, [help, currentQuestion, emptyIndexes]);
+
+  return useMemo(
+    () => ({ resetHelp, help, getQuestionHelp }),
+    [resetHelp, help, getQuestionHelp]
+  );
+};
+
+export default useHelp;
--- packages/client/src/contexts/useQuizActions.ts
+++ packages/client/src/contexts/useQuizActions.ts
@@ -2,6 +2,7 @@ import { useCallback, useMemo, useState } from "react";
 import { EQuizStatus } from "../types/Quiz";
 import useQuizApi from "./useQuizApi";
 import { AnsweredQuestion } from "../types/Question";
+import useHelp from "./useHelp";
 
 const useQuizActions = () => {
   const {
@@ -15,7 +16,40 @@ const useQuizActions = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
   const [score, setScore] = useState<number[]>([]);
   const [answers, setAnswers] = useState<string[]>([]);
-  const [help, setHelp] = useState<string[]>([]);
+
+  const currentQuestion = useMemo(
+    () => questions[currentQuestionIndex],
+    [questions, currentQuestionIndex]
+  );
+  const currentAnswer = useMemo(
+    () => answers[currentQuestionIndex] || "",
+    [currentQuestionIndex, answers]
+  );
+
+  const setCurrentQuestionScore = useCallback(
+    (isCorrectAnswer: boolean, withHelp: boolean) => {
+      const questionScore = isCorrectAnswer ? (withHelp ? 1 : 2) : -1;
+      setScore((prev) => {
+        const next = [...prev];
+        next[currentQuestionIndex] = questionScore;
+        return next;
+      });
+    },
+    [currentQuestionIndex]
+  );
+
+  const goToNextQuestion = useCallback(() => {
+    if (currentQuestionIndex + 1 >= questions.length) {
+      setStatus(EQuizStatus.DONE);
+    }
+    setCurrentQuestionIndex((prev) => prev + 1);
+  }, [currentQuestionIndex, questions]);
+
+  const { resetHelp, help, getQuestionHelp } = useHelp(
+    currentQuestion,
+    setCurrentQuestionScore,
+    goToNextQuestion
+  );
 
   const startQuiz = useCallback(async () => {
     const apiQuestions = await getQuestionsApiCall();
@@ -28,23 +62,17 @@ const useQuizActions = () => {
     setCurrentQuestionIndex(0);
     setAnswers([]);
     setScore([]);
+    resetHelp();
     setStatus(EQuizStatus.IN_PROGRESS);
-    setHelp([]);
   }, []);
 
   const answerQuestion = useCallback(async () => {
-    const currentAnswer = answers[currentQuestionIndex];
-    if (
-      status !== EQuizStatus.IN_PROGRESS ||
-      !questions ||
-      currentQuestionIndex > questions.length ||
-      !currentAnswer
-    ) {
+    if (status !== EQuizStatus.IN_PROGRESS || !currentAnswer) {
       return;
     }
 
     const isCorrectAnswer = await answerQuestionApiCall(
-      questions[currentQuestionIndex].id,
+      currentQuestion.id,
       currentAnswer
     );
 
@@ -52,72 +80,17 @@ const useQuizActions = () => {
       return;
     }
 
-    const hasHelp = Boolean(help.length);
-    const questionScore = isCorrectAnswer ? (hasHelp ? 1 : 2) : -1;
-    setScore((prev) => {
-      const next = [...prev];
-      next[currentQuestionIndex] = questionScore;
-      return next;
-    });
-
-    // last question
-    if (currentQuestionIndex + 1 === questions.length) {
-      setStatus(EQuizStatus.DONE);
-      return;
-    }
-
-    setCurrentQuestionIndex((prev) => prev + 1);
-    setHelp([]);
-  }, [status, questions, currentQuestionIndex, answers]);
-
-  const getQuestionHelp = useCallback(() => {
-    const currentQuestion = questions[currentQuestionIndex];
-
-    // just show number of letters
-    if (!help.length) {
-      setHelp(
-        [...Array(currentQuestion.correctAnswer.length)].map((_, i) => {
-          // duplicate spaces between words
-          if (currentQuestion.correctAnswer[i] === " ") {
-            return " ";
-          }
-          return "";
-        })
-      );
-      return;
-    }
-
-    const emptyIndexes = help.reduce(
-      (prevIndexes: number[], currentHelp, currentIndex) => {
-        const nextIndexes = [...prevIndexes];
-
-        if (!currentHelp) {
-          nextIndexes.push(currentIndex);
-        }
-
-        return nextIndexes;
-      },
-      []
-    );
-
-    if (!emptyIndexes.length) {
-      return;
-    }
-
-    const randomIndexInRange = Math.floor(Math.random() * emptyIndexes.length);
-    const randomEmptyIndex = emptyIndexes[randomIndexInRange];
-
-    setHelp((prev) => {
-      const next = [...prev];
-      next[randomEmptyIndex] = currentQuestion.correctAnswer[randomEmptyIndex];
-      return next;
-    });
-  }, [help, questions, currentQuestionIndex]);
-
-  const currentAnswer = useMemo(
-    () => answers[currentQuestionIndex] || "",
-    [currentQuestionIndex, answers]
-  );
+    setCurrentQuestionScore(isCorrectAnswer, Boolean(help.length));
+    goToNextQuestion();
+    resetHelp();
+  }, [
+    status,
+    currentQuestion,
+    currentAnswer,
+    goToNextQuestion,
+    help,
+    resetHelp,
+  ]);
 
   const setCurrentAnswer = useCallback(
     (answer: string) => {
