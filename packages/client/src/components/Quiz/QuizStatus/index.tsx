14f847e1c1cc1da4d6ad198746bfe6911127f140
Filip Sakala <filip.sakala@vacuumlabs.com>
2024-05-24 21:52:41 +0200

    fix: unused imports

--- packages/client/src/components/Quiz/QuizStatus/index.tsx
+++ packages/client/src/components/Quiz/QuizStatus/index.tsx
@@ -1,20 +1,7 @@
 import { QuizContext } from "../../../contexts/QuizContextProvider";
 import { EQuizStatus } from "../../../types/Quiz";
-import { styled } from "@mui/material";
 import QuizInProgressStatus from "./QuizInProgressStatus";
 import { useContextSelector } from "use-context-selector";
-
-const StyledWrapper = styled("div")(
-  ({ theme }) => `
-      display: flex;
-      gap: 5px;
-
-      align-items: center;
-      padding: 10px;
-
-      border-bottom: 1px solid ${theme.palette.divider};
-`
-);
 
 const QuizStatus = () => {
   const status = useContextSelector(QuizContext, (c) => c.quizStatus);
